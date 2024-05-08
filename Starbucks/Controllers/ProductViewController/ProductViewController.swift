1286efee903a7f909a3443b8288fb927b26049ba
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-05-09 00:05:10 +0200

    fixed some constraints issues and lagging of products when navigating to another screens

--- Starbucks/Controllers/ProductViewController/ProductViewController.swift
+++ Starbucks/Controllers/ProductViewController/ProductViewController.swift
@@ -25,17 +25,18 @@ class ProductViewController: UIViewController, UITableViewDelegate, UITableViewD
     lazy var addToBasketButton: UIButton = {
         let button = UIButton()
         button.isEnabled = false
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Add to basket • R48.00", for: .normal)
         button.layer.cornerRadius = 8.0
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = StarbucksColors.primaryColor
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Latte"
+        view.backgroundColor = .white 
         setupUI()
         registerCell()
         configureHeaderView()
@@ -49,13 +50,13 @@ class ProductViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         productTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         productTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        productTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        productTableView.bottomAnchor.constraint(equalTo: addToBasketButton.topAnchor, constant: -20).isActive = true
         productTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         
-        addToBasketButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32).isActive = true
+        addToBasketButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
         addToBasketButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         addToBasketButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        addToBasketButton.heightAnchor.constraint(equalToConstant: 42).isActive = true
+        addToBasketButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
         
     }
     
