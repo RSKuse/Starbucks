cf474e44ca6ee2021a712795acf0d3b6e2093dd4
Gugulethu <mhlangagc@gmail.com>
2024-06-13 08:41:36 +0200

    Created a simpler version of the json conversion with less complex code.

--- Starbucks/JsonToSwiftConversion.swift
+++ Starbucks/JsonToSwiftConversion.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 
-final class ConvertJsonToModel {
 
+class ConvertJsonToModel {
+    
+    static func createStarBucksCategory() -> Categories? {
+        
+        let path = Bundle(for: self).path(forResource: "StarbucksCategories", ofType: "json")
+        let decoder = JSONDecoder()
+        
+        do {
+            let jsonData = try Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped)
+            let categories = try decoder.decode(Categories.self, from: jsonData)
+            return categories
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+}
+
+/**
+ - An Advanced Way of Doing it
+ */
+/*
+class JsonConversion {
     class func convert<Model: Codable>(fromFileName file: String) -> Model? {
         
         do {
@@ -27,3 +49,4 @@ final class ConvertJsonToModel {
         return try Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped)
     }
 }
+*/
